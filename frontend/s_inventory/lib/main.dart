6094573dd96b26bf2e4d9b3832271c5fc96a8368
andreslandines <cardosoandres331@gmail.com>
2026-09-18 10:28:04 -0500

    Adelanto de una pantalla de dart

--- frontend/s_inventory/lib/main.dart
+++ frontend/s_inventory/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:s_inventory/pantallas/inicio.dart';
+import 'package:s_inventory/pantallas/recoverypassword.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,7 +14,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: inicio()
+        body: Recoverypassword()
       ),
     );
   }
