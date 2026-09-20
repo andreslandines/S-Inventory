f7d3fd7ed2a3ebaeae8756250ee5c2634fe07587
Sofía <sofiatovar2965@gmail.com>
2026-09-19 20:42:32 -0500

    carpetas

--- frontend/s_inventory/lib/main.dart
+++ frontend/s_inventory/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:s_inventory/pantallas/inicio.dart';
 import 'package:s_inventory/pantallas/recoverypassword.dart';
+=======
+import 'package:s_inventory/pantallas/iniciologo.dart';
+import 'package:s_inventory/pantallas/login.dart';
+>>>>>>> b9b48df (carpetas)
 
 void main() {
   runApp(const MainApp());
@@ -11,10 +16,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+     return const MaterialApp(
+      debugShowCheckedModeBanner:false ,
       home: Scaffold(
+<<<<<<< HEAD
         body: Recoverypassword()
+=======
+        body:SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              iniciologo(),
+              login(),
+              
+
+            ],
+          ),
+        ),
+>>>>>>> b9b48df (carpetas)
       ),
     );
   }
